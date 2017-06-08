b6bf392d4d411142e1bfad76322a26d0c3b473b0
Ryan Kotzen <ryan@encentivize.co.za>
2017-06-08 14:36:00 +0200

    updating config to read from webpack.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
-    entry: "./src/index.tsx",
+    entry: "./js-src/index.js",
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
