9ce8a330083d5f0514aee912dde00d32ceb9aa08
Ryan Kotzen <ryan@encentivize.co.za>
2017-06-08 20:47:02 +0200

    more conversions

--- js-src/index.js
+++ js-src/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
-//--------------------------------------------------- [Start index.tsx]
 import React from 'react';
 import RX from 'reactxp';
 import App from './App';
 RX.App.initialize(true, true);
 RX.UserInterface.setMainView(<App />);
-//--------------------------------------------------- [End index.tsx] 
